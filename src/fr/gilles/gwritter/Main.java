04873ad16ca99edc59eb6bd2f57a0d8b45eaa026
Gilles-kpn <76532932+Gilles-kpn@users.noreply.github.com>
2021-01-16 17:24:59 +0100

    Update Main.java

--- src/fr/gilles/gwritter/Main.java
+++ src/fr/gilles/gwritter/Main.java
@@ -3,7 +3,6 @@ package fr.gilles.gwritter;
 public class Main {
 	public static Fenetre f;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		 f = new Fenetre();
 		f.setVisible(true);
 	}
